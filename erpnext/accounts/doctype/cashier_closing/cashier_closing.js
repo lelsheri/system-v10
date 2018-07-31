7077dfb97c3d90a28a0b718b7b6547185128e8db
elba7r <elba7r@gmail.com>
2018-07-31 03:53:35 +0200

    30-7-2018

--- erpnext/accounts/doctype/cashier_closing/cashier_closing.js
+++ erpnext/accounts/doctype/cashier_closing/cashier_closing.js
@@ -3,9 +3,19 @@
 
 frappe.ui.form.on('Cashier Closing', {
 
+	setup: function(frm){
+		var me = this;
+		frm.doc.user = user
+	},
+
+	refresh: function(frm){
+		var me = this;
+		frm.doc.user = user
+	},
+
 	in_save: function(frm){
 		var me = this;
 
-		frm.doc.net_amount = flt(frm.doc.in_save) - flt(frm.doc.custody)
+		frm.doc.net_amount = flt(frm.doc.in_save) - flt(frm.doc.custody) + flt(frm.doc.expense)
 	}
 });
