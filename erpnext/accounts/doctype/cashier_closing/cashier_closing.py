f8edf2bd591905d869eac6db74f9823c97f3f87c
elba7r <elba7r@gmail.com>
2018-10-22 00:51:51 +0200

    21-10-2018

--- erpnext/accounts/doctype/cashier_closing/cashier_closing.py
+++ erpnext/accounts/doctype/cashier_closing/cashier_closing.py
@@ -27,7 +27,7 @@ class CashierClosing(Document):
 		for i in self.payments:
 			total += i.amount
 
-		self.net_amount = total + self.outstanding_amount + self.expense - self.custody
+		self.net_amount = total + self.outstanding_amount + self.expense - self.custody + self.returns
 
 	def validate_time(self):
 		if self.from_time >= self.time:
